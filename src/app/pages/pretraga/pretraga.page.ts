d7e3872a5425fc06c5ac5c70b6c50f94f4d1aa50
nzmnisam <milostaptap@gmail.com>
2020-04-22 17:54:31 +0200

    Bottom bar i meni za nalog

---
+++ src/app/pages/pretraga/pretraga.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pretraga',
+  templateUrl: './pretraga.page.html',
+  styleUrls: ['./pretraga.page.scss'],
+})
+export class PretragaPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/pages/proizvodi/proizvodi.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-proizvodi',
+  templateUrl: './proizvodi.page.html',
+  styleUrls: ['./proizvodi.page.scss'],
+})
+export class ProizvodiPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
